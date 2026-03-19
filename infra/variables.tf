b0e69f7dd43f7ad07beadf09c6f1f316bcce4a5c
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-03-19 11:45:06 +0000

    Add variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  description = "AWS region for the infrastructure"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "gatus"
+}
+
+variable "domain_name" {
+  description = "Fully qualified domain for the app"
+  type        = string
+  default     = "gatus.moabdullahi.uk"
+}
+
+variable "hosted_zone_id" {
+  description = "Route53 hosted zone ID for the delegated subdomain"
+  type        = string
+}
