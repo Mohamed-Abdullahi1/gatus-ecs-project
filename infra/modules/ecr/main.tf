5c5958163866e08df86eb1b33ec3df4edc497ba1
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-03-23 13:21:03 +0000

    Enable force delete for ECR repository

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,8 @@
 resource "aws_ecr_repository" "gatus" {
   name                 = var.project_name
   image_tag_mutability = var.image_tag_mutability
+  
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
