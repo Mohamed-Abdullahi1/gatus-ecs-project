8372b3bfa4de02b5d7fcf9bb47978b2514d346eb
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-03-24 11:18:49 +0000

    Introduce bootstrap infrastructure for ECR to break CI/CD dependency loop

---
+++ bootstrap/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "gatus"
+}
+
+variable "image_tag_mutability" {
+  type    = string
+  default = "MUTABLE"
+}
