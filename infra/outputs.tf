25580deffb2d2d8151b3fec0c66edf113f56029e
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-03-24 11:27:20 +0000

    Removed ECR output from infra

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,11 +8,6 @@ output "certificate_arn" {
   value       = module.acm.certificate_arn
 }
 
-output "ecr_repository_url" {
-  description = "ECR repository URL"
-  value       = module.ecr.repository_url
-}
-
 output "ecs_service_name" {
   description = "Name of the ECS service"
   value       = module.ecs.service_name
