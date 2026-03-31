c3dfe230d8994f25f1e5249e6cbc05f1c8d1eaa6
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-03-31 11:40:39 +0100

    Update SSL policy

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -87,7 +87,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.gatus_alb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.certificate_arn
 
   default_action {
