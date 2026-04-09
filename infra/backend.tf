57055fc5037a820d77441698eff7af94c52e624f
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-09 11:33:46 +0100

    restore backend bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "gatus-tf-state-REDACTED"
+    bucket       = "gatus-tf-state-454374565233"
     key          = "ecs-project/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
