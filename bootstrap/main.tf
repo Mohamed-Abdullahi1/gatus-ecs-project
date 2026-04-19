d8cd8fa6201defa298d5d59d5d589f8b1d43da11
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-04-19 13:51:34 +0100

    format Terraform files

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "gatus" {
   name                 = var.project_name
   image_tag_mutability = var.image_tag_mutability
-  
+
   force_delete = true
 
   image_scanning_configuration {
